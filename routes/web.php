c5b16d6b8332dd6fd24d79bac0c2bd3ad4d5a21c
sandysormin19 <sandysormin9@gmail.com>
2025-05-19 20:54:49 +0700

    admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\AdminController;
 Route::get('/', function () {
     return view('welcome');
+});
+Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function(){
+Route::match(['get', 'post'], 'dashboard',[AdminController::class,'dashboard']);
 });
