e02196b8e46435df613dc1c2a67de67f0799f21e
Abyys24 <masterdoesx@gmail.com>
2025-06-02 15:51:45 +0700

    Menghilangkan fitur tidak terpakai

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -22,59 +22,6 @@ class AdminController extends Controller
         return view('admin.dashboard', compact('stocks', 'totalOrders', 'totalRevenue', 'orderList'));
     }
 
-    public function login(Request $request)
-    {
-        if ($request->isMethod('post')) {
-            $admin = Admin::where('email', $request->email)->first();
-
-            if ($admin && Hash::check($request->password, $admin->password)) {
-                // Set session manual
-                session(['is_admin' => true, 'admin_id' => $admin->_id, 'admin_name' => $admin->name]);
-                AdminLogin::create([
-                    'admin_id'   => $admin->_id,
-                    'admin_name' => $admin->name,
-                    'email'      => $admin->email,
-                    'ip_address' => $request->ip(),
-                    'login_at'   => now(),
-                ]);
-                return redirect('/admin/dashboard');
-            }
-
-            return back()->withErrors(['email' => 'Email atau password salah.']);
-        }
-
-        return view('admin.login');
-    }
-
-   // public function register(Request $request)
-  //  {
- //    if ($request->isMethod('post')) {
-//      $request->validate([
-//        'name' => 'required|string|max:255',
- //      'email' => 'required|email|unique:admins,email',
-   //    'password' => 'required|string|min:6|confirmed',
-  // ]);
- // Admin::create([
-//   'name' => $request->name,
-// 'email' => $request->email,
- //'password' => Hash::make($request->password),
-//]);
-
-      //      return redirect('/admin/login')->with('success', 'Akun admin berhasil dibuat.');
-      //  }
-
-    //    return view('admin.register');
-  //  }
-
-    public function logout(Request $request)
-    {
-        // Hapus session manual
-        $request->session()->forget(['is_admin', 'admin_id', 'admin_name']);
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-        return redirect('/admin/login');
-    }
-
     public function stok()
     {
         $types = ['3kg', '12kg'];
